b836ffc315a6e011a82cf3bd0faef07f54d9b5b8
Imron jehleh <imron.jehleh@gmail.com>
2024-08-23 19:46:23 +0700

    Add index.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  root "landingpage#index"
 
   # Defines the root path route ("/")
   # root "posts#index"
