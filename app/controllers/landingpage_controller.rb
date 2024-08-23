218ad7375bbebe8734de0846154a8295ecefcda9
Imron jehleh <imron.jehleh@gmail.com>
2024-08-23 19:52:41 +0700

    Add variable.

--- app/controllers/landingpage_controller.rb
+++ app/controllers/landingpage_controller.rb
@@ -1,4 +1,6 @@
 class LandingpageController < ApplicationController
   def index
+    @owner = "Imron Jehleh"
+    @organization = "Department of Computer Engineering, Faculty of Engineering, Ramkhamhaeng University"
   end
 end
