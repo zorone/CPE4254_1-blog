a166d2117821cbdbffa0b45b940ddb7c61460b0f
Imron jehleh <imron.jehleh@gmail.com>
2024-08-31 11:04:11 +0700

    Okay, now I fully understand what's go wrong.
    There are function sites() and class Site.
    
    To access function, concatenate with dot. Ex. Jenkyll.sites
    To access class, concatenate with ::, Ex. Jenkyll:Site
    
    For style warning "Do not use `::` for method calls." It becauses I put Jenkyll::site.new instead of Jenkyll::Site.new
    Seems like Upper Case in Ruby has meaning...

--- config/initializers/web.rb
+++ config/initializers/web.rb
@@ -7,7 +7,7 @@ Rails.application.config.after_initialize do
     dest = Rails.root.join("public/web")
 
     # generate the site
-    Jekyll.sites.new(
+    Jekyll::Site.new(
       Jekyll.configuration({
         "config" => Rails.root.join("config", "jekyll.yml").to_s,
         "source" => Rails.root.join("web").to_s,
@@ -18,6 +18,6 @@ Rails.application.config.after_initialize do
     # the strange codes give the output color
     Rails.logger.info "\e[0;32;49mJekyll site built!\e[0m]]"
   rescue => e
-    Rails.logger.error "\e[0;31;49mJekyll site build failed.\e[0m]]\n\e[0;33;49mError:\e[0m #{e}]]"
+    Rails.logger.error "\e[0;31;49mJekyll site build failed.\e[0m\n\e[0;33;49mError:\e[0m #{e}"
   end
 end
