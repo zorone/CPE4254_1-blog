cdd1036c7f3359776bc48aa6e40bac9e8dcaa880
Imron jehleh <imron.jehleh@gmail.com>
2024-08-31 10:47:23 +0700

    Still wrong.

--- config/initializers/web.rb
+++ config/initializers/web.rb
@@ -18,6 +18,6 @@ Rails.application.config.after_initialize do
     # the strange codes give the output color
     Rails.logger.info "\e[0;32;49mJekyll site built!\e[0m]]"
   rescue => e
-    Rails.logger.error "\e[0;31;49mJekyll site build failed.\e[0m\n\e[0;33;49mError:\e[0m #{e}]]]]"
+    Rails.logger.error "\e[0;31;49mJekyll site build failed.\e[0m]]\n\e[0;33;49mError:\e[0m #{e}]]"
   end
 end
